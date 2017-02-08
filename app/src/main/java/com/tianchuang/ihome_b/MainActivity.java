83becb9414f6902c21f5ce7b2841c2d94cd92d44
龚世晟 <gongshisheng@renhenet.com.cn>
2017-02-08 19:48:31 +0800

    初始化

--- app/src/main/java/com/tianchuang/ihome_b/MainActivity.java
+++ app/src/main/java/com/tianchuang/ihome_b/MainActivity.java
@@ -3,6 +3,8 @@ package com.tianchuang.ihome_b;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tianchuang.myapplication.R;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
