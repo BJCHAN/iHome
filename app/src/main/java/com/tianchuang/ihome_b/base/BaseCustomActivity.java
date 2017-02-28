0106916b49caa217087adf34dd85907a6a52f946
wujian <wujian@renhenet.com.cn>
2017-02-28 15:03:40 +0800

    完善BaseCustonactivity

--- app/src/main/java/com/tianchuang/ihome_b/base/BaseCustomActivity.java
+++ app/src/main/java/com/tianchuang/ihome_b/base/BaseCustomActivity.java
@@ -17,59 +17,50 @@ import butterknife.ButterKnife;
 public abstract class BaseCustomActivity extends BaseActivity {
 
 
-	private Boolean isFinishWithAnim = false;//退出是否加入关闭动画
+    private Boolean isFinishWithAnim = false;//退出是否加入关闭动画
 
-	public void setFinishWithAnim(Boolean finishWithAnim) {
-		isFinishWithAnim = finishWithAnim;
-	}
+    public void setFinishWithAnim(Boolean finishWithAnim) {
+        isFinishWithAnim = finishWithAnim;
+    }
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(getLayoutId());
-		ButterKnife.bind(this);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+    }
 
-	@Override
-	protected void removeFragment() {
-		if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
-			FragmentUtils.popFragment(getSupportFragmentManager());
-		} else {
-			customFinish();
-		}
-	}
+    @Override
+    protected void removeFragment() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+            FragmentUtils.popFragment(getSupportFragmentManager());
+        } else {
+            customFinish();
+        }
+    }
 
-	//返回键返回事件
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (KeyEvent.KEYCODE_BACK == keyCode) {
-			if (getSupportFragmentManager().getBackStackEntryCount() <= 1) {
-				customFinish();
-				return true;
-			}
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+    //返回键返回事件
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (KeyEvent.KEYCODE_BACK == keyCode) {
+            if (getSupportFragmentManager().getBackStackEntryCount() <= 1) {
+                customFinish();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
-	/**
-	 * 自定义的关闭方式
-	 */
-	private void customFinish() {
-		if (isFinishWithAnim) {
-			finishWithAnim();
-		} else {
-			finish();
-		}
-	}
-
-	/**
-	 * 关闭带动画
-	 */
-	private void finishWithAnim() {
-		finish();
-		overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
-	}
-
+    /**
+     * 自定义的关闭方式
+     */
+    private void customFinish() {
+        finish();
+        if (isFinishWithAnim) {
+            //关闭带动画
+            overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
+        }
+    }
 
 }
